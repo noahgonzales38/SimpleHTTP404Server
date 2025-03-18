d7654034c58e3b8c8f8865ae157e8c00473809a9
noahgonzales38 <91799787+noahgonzales38@users.noreply.github.com>
2025-03-17 20:06:41 -0700

    update

--- SimpleHTTP404Server.py
+++ SimpleHTTP404Server.py
@@ -1,14 +1,12 @@
 import os
-import SimpleHTTPServer
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-
-class SimpleHTTP404RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+class SimpleHTTP404RequestHandler(SimpleHTTPRequestHandler):
     """
     Overrides the default request handler to handle custom 404 pages as 404.html
     (i.e. a 404.html page located in the root). This behavior is seen on:
         GitHub:     https://help.github.com/articles/custom-404-pages/
         FastMail:   https://www.fastmail.com/help/files/website.html
-
     """
     def do_GET(self):
         path = self.translate_path(self.path)
@@ -18,8 +16,12 @@ class SimpleHTTP404RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             self.path = '404.html'
 
         # Call the superclass methods to actually serve the page.
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+        super().do_GET()
 
 
 if __name__ == '__main__':
-    SimpleHTTPServer.test(SimpleHTTP404RequestHandler)
+    # Create the HTTP server on a specific address and port
+    server_address = ('', 8000)  # Serve on all available interfaces on port 8000
+    httpd = HTTPServer(server_address, SimpleHTTP404RequestHandler)
+    print("Serving on port 8000...")
+    httpd.serve_forever()
